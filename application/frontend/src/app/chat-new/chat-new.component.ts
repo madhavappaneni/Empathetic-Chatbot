42fda92d6aceb50082e09aad7fa9e98ba2e5f60e
madhavappaneni <appanenim@gmail.com>
2023-05-10 17:14:23 -0400

    chat_bot_response

--- application/frontend/src/app/chat-new/chat-new.component.ts
+++ application/frontend/src/app/chat-new/chat-new.component.ts
@@ -21,7 +21,7 @@ export class ChatNewComponent implements OnInit {
     this.appService.get_response(data).subscribe((response: any) => {
       this.appService.messageArray.push({
         name: 'bot',
-        message: response.reranked_response,
+        message: response.chat_bot_response,
       });
     });
     this.message = '';
